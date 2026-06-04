923e89280b102b602b6f90ea49f570666b8ee5fc
yashonidhiThakur <yashonidhithakur@gmail.com>
2026-06-04 18:42:48 +0530

    Added aws RI purchase req and response DTOs

--- services/chaos-api/src/main/java/io/finflow/chaosapi/aws/dto/PurchaseReservedInstancesResponse.java
+++ services/chaos-api/src/main/java/io/finflow/chaosapi/aws/dto/PurchaseReservedInstancesResponse.java
@@ -1,4 +1,20 @@
 package io.finflow.chaosapi.aws.dto;
 
-public class PurchaseReservedInstancesResponse {
-}
+/**
+ * The response payload (DTO) for a successful AWS EC2 Reserved Instance purchase.
+ *
+ * What this does - Response to the purchase request
+ * After the API successfully processes a purchase request, it returns this object
+ * containing a freshly generated mock AWS Reservation ID and a status string.
+ *
+ * Why it exists - The Saga & Fault Injection
+ * This provides the successful confirmation for the FinFlow rebalance saga.
+ * Later in the project (Day 4), fault injection will be added to this endpoint to
+ * randomly fail the request, to test the saga's automatic
+ * rollback/compensation mechanisms in a simulated crisis.
+ */
+
+public class PurchaseReservedInstancesResponse(
+        String reservationId,
+        String state
+) {}
