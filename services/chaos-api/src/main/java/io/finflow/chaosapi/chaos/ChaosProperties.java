5377b37204911435779ca428b4bcaafb32545957
yashonidhiThakur <yashonidhithakur@gmail.com>
2026-06-07 05:53:49 +0530

    Created Java rep of Config file settings

--- services/chaos-api/src/main/java/io/finflow/chaosapi/chaos/ChaosProperties.java
+++ services/chaos-api/src/main/java/io/finflow/chaosapi/chaos/ChaosProperties.java
@@ -1,4 +1,20 @@
 package io.finflow.chaosapi.chaos;
 
-public class ChaosProperties {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "chaos")
+public record ChaosProperties (
+    boolean enabled,
+    int faultRate,
+    double hangShare,
+    long hangMillis
+) {
+    public ChaosProperties{
+        if(faultRate<0 || faultRate>100){
+            throw new IllegalArgumentException("Chaos.fault-rate must be between 0-100, it was " + faultRate);
+        }
+        if(hangShare<0.0 || hangShare>1.0){
+            throw new IllegalArgumentException("Chaos,hang-share must be between 0.0-1.0, it was " + hangShare);
+        }
+    }
 }
